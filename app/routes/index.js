070a92c7e14d6a3eecd2d05c012d19d0da24bb4d
plauer <plauer411@gmail.com>
2014-12-06 12:52:38 -0700

    chat messages appear on page. refactoring to render partials

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var chat = " <div class = 'chat-div'><div class='chat'></div><input type='text' id='nameInput' placeholder='Name'><input type='text' id='messageInput' placeholder='Message'></div>"
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Kitchen' , chat: chat});
+  res.render('index', { title: 'Kitchen'});
 });
 
 module.exports = router;
