ce1413091f3e7717800e37a62dc5d08dfc4171a0
Rory <roryc89@googlemail.com>
2014-12-06 18:41:55 +0000

    this modularizes the zebra code

---
+++ app/public/javascripts/zebraFiles/zebraMain.js
@@ -0,0 +1,18 @@
+// This module creates a basic zebra canvas and uses the zebra modules to create the kitchen layout
+zebra.ready(function(){
+
+  eval(zebra.Import("ui", "layout"));
+
+
+
+  // build Zebra canvas component that adds new
+  // Canvas DOM element into page with the given size
+  var zCanvas = new zebra.ui.zCanvas(1300,600);
+  var root    = zCanvas.root; // save reference to root UI component
+  root.setBorder(new zebra.ui.Border("lightblue", 3 , 20));
+
+  createTables(root);
+
+
+});
+
