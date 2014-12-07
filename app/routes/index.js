a879e5d459692fa02eed9c4065cee80561968d92
plauer <plauer411@gmail.com>
2014-12-06 17:26:27 -0700

    gets rid of extra jquery cdn. adds a couple comments

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+//DOM element that renders chat form
 var chat = "<div class = 'chat-div'><div class='chat'></div><input type='text' id='nameInput' placeholder='Name'><input type='text' id='messageInput' placeholder='Message'></div>";
 /* GET home page. */
 router.get('/', function(req, res) {
